a238f06f23a27a41b49179457adbb2d668fae828
Computhink-CS126 <cs126@computhink.com.sg>
2025-08-30 07:12:24 +0000

    Aug 30, 2025, 7:12 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,12 @@
-
+function setup() {
+    createCanvas(600, 400);
+    background(0, 0, 0);
+}
+function draw(){
+    stroke("red");
+    strokeWeight("10");
+    fill("black")
+    circle (120, 200, 100);
+    triangle(240, 250, 360, 250, 300, 150);
+    rect(430, 150, 100, 100);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,12 @@
+function setup() {
+    createCanvas(600, 600);
+    background(220);
+}
 
+function draw() {
+let circleColour = 255 ; 
+    for ( let i = 1 ; i < 11 ; i++ ) {
+        fill(255, 255 , (circleColour - (25.5 * i)));
+        circle ((50 * i), (50 * i), 50);
+    }
+}
